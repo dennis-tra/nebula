2d7b7fc9a7d9f2412712b8864282253d9ab022b1
Dennis Trautwein <git@dtrautwein.eu>
2021-07-10 22:09:29 +0200

    Add geoip analysis script

--- data/geoip/script.js
+++ data/geoip/script.js
@@ -1,13 +1,41 @@
-// Synchronous database opening
-const fs = require('fs');
-const Reader = require('@maxmind/geoip2-node').Reader;
+const postgres = require("postgres");
+const { Multiaddr } = require("multiaddr");
+const fs = require("fs");
+const Reader = require("@maxmind/geoip2-node").Reader;
 
-const dbBuffer = fs.readFileSync('GeoLite2-Country.mmdb');
-
-// This reader object should be reused across lookups as creation of it is
-// expensive.
+const dbBuffer = fs.readFileSync("GeoLite2-Country.mmdb");
 const reader = Reader.openBuffer(dbBuffer);
 
-response = reader.country('188.63.76.112');
+const sql = postgres("postgres://nebula:password@localhost:5432/nebula");
 
-console.log(response.country.isoCode);
+const countries = { unknown: [] };
+
+const main = async () => {
+    const peers = await sql`select * from peers`;
+    for (const peer of peers) {
+        let found = false;
+        for (const maddrStr of peer.multi_addresses) {
+            try {
+                const maddr = new Multiaddr(maddrStr);
+                console.log(maddr);
+                const response = reader.country(maddr.nodeAddress().address);
+                const iso = response.country.isoCode;
+                if (countries[iso] === undefined) {
+                    countries[iso] = 0;
+                }
+                countries[iso]++;
+                found = true;
+                break;
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        if (!found) {
+            countries["unknown"].push(peer.multi_addresses);
+        }
+    }
+    console.log(countries);
+    return;
+};
+
+main();
