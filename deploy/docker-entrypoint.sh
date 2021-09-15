b8387178579e14001be4af3c08fb8b7ae20d4be2
wcgcyx <wcgcyx@gmail.com>
2021-09-15 13:00:53 +1000

    Update

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -7,14 +7,46 @@ set -e
 : "${NEBULA_DATABASE_NAME:=nebula}"
 : "${NEBULA_DATABASE_USER:=nebula}"
 : "${NEBULA_DATABASE_PASSWORD:=password}"
+: "${CRAWL_PERIOD:=1800}"
+: "${CRAWL_NEIGHBOUR_SAVE:=1}"
+: "${CRAWL_NEIGHBOUR_FREQUENCY:=48}"
+: "${CRAWL_NEIGHBOUR_TRUNCATE:=1}"
 
 migrate -database "postgres://$NEBULA_DATABASE_USER:$NEBULA_DATABASE_PASSWORD@$NEBULA_DATABASE_HOST:$NEBULA_DATABASE_PORT/$NEBULA_DATABASE_NAME?sslmode=disable" -path migrations up
 
+period=$CRAWL_PERIOD
+save=$CRAWL_NEIGHBOUR_SAVE
+freq=$CRAWL_NEIGHBOUR_FREQUENCY
+truncate=$CRAWL_NEIGHBOUR_TRUNCATE
+
+monitor=0
+counter=1
 while true; do
     start=$(date +%s)
-    nebula --prom-port=6667 crawl 
+    if [ $save -eq 1 ]
+    then 
+        if [ $counter -eq $freq ]
+        then
+            if [ $truncate -eq 1 ]
+            then
+                echo "truncate db..."
+            fi
+            nebula --prom-port=6666 crawl --save-neighbour
+            counter=1
+        else
+            nebula --prom-port=6666 crawl
+            counter=$((counter + 1))
+        fi
+    else
+        nebula --prom-port=6666 crawl
+    fi
+    if [ $monitor -eq 0 ]
+    then
+        nebula --prom-port=6667 monitor &
+        monitor=1
+    fi
     end=$(date +%s)
     echo $(date)
     d=$((end - start))
-    sleep $((1800 - d))
+    sleep $((period - d))
 done
