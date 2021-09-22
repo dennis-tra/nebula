08f70bbb2c862d058cd817f6c4a33ca21dc560e9
Dennis Trautwein <git@dtrautwein.eu>
2021-09-22 14:20:28 +0200

    Add Pegasys analysis scripts

---
+++ analysis/mixed/lib/node_classification.py
@@ -0,0 +1,101 @@
+import pytz
+
+
+# get_all_nodes gets the id of all nodes between two timestamps.
+# It takes an sql connection, the start time, the end time as the arguments, and
+# returns the ids of all the nodes.
+def get_all_nodes(conn, start, end):
+    start = start.astimezone(pytz.utc)
+    end = end.astimezone(pytz.utc)
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT DISTINCT peer_id
+        FROM sessions
+        WHERE created_at < %s AND updated_at > %s
+        """,
+        [end, start]
+    )
+    return [i for sub in cur.fetchall() for i in sub]
+
+
+# get_on_nodes gets the id of all on nodes between two timestamps.
+# It takes an sql connection, the start time, the end time as the arguments, and
+# returns the ids of all the on nodes.
+def get_on_nodes(conn, start, end):
+    start = start.astimezone(pytz.utc)
+    end = end.astimezone(pytz.utc)
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT DISTINCT peer_id
+        FROM sessions
+        WHERE (created_at < %s AND updated_at > %s AND first_successful_dial != last_successful_dial) OR (created_at < %s AND finished = false) AND peer_id NOT IN (
+            SELECT peer_id
+            FROM sessions
+            WHERE updated_at > %s AND updated_At < %s AND finished = true
+        )
+        """,
+        [end, end, end, start, end]
+    )
+    return [i for sub in cur.fetchall() for i in sub]
+
+
+# get_off_nodes gets the id of all off nodes between two timestamps.
+# It takes an sql connection, the start time, the end time as the arguments, and
+# returns the ids of all the off nodes.
+def get_off_nodes(conn, start, end):
+    start = start.astimezone(pytz.utc)
+    end = end.astimezone(pytz.utc)
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT DISTINCT peer_id
+        FROM sessions
+        WHERE created_at < %s AND updated_at > %s AND first_successful_dial = last_successful_dial AND finished = true AND peer_id NOT IN (
+            SELECT peer_id
+            FROM sessions
+            WHERE updated_at > %s AND updated_At < %s AND first_successful_dial != last_successful_dial AND finished = true
+        )
+        """,
+        [end, start, start, end]
+    )
+    return [i for sub in cur.fetchall() for i in sub]
+
+
+# get_dangling_nodes gets the id of all dangling nodes between two timestamps.
+# It takes an sql connection, the start time, the end time as the arguments, and
+# returns the ids of all the dangling nodes.
+def get_dangling_nodes(conn, start, end):
+    start = start.astimezone(pytz.utc)
+    end = end.astimezone(pytz.utc)
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT DISTINCT peer_id
+        FROM sessions
+        WHERE updated_at > %s AND updated_at < %s AND first_successful_dial != last_successful_dial AND finished = true
+        """,
+        [start, end]
+    )
+    return [i for sub in cur.fetchall() for i in sub]
+
+
+# get_highly_dangling_nodes gets the id of all highly dangling nodes between two timestamps.
+# It takes an sql connection, the start time, the end time, the number of sessions as the arguments, and
+# returns the ids of all the highly dangling nodes.
+def get_highly_dangling_nodes(conn, start, end, num):
+    start = start.astimezone(pytz.utc)
+    end = end.astimezone(pytz.utc)
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT peer_id
+        FROM sessions
+        WHERE updated_at > %s AND updated_at < %s AND first_successful_dial != last_successful_dial AND finished = true
+        GROUP BY peer_id
+        HAVING COUNT(*) > %s
+        """,
+        [start, end, num]
+    )
+    return [i for sub in cur.fetchall() for i in sub]
---
+++ analysis/mixed/plot_icmp_ping.py
@@ -0,0 +1,32 @@
+import psycopg2
+import toml
+import matplotlib.pyplot as plt
+from lib import node_time, node_classification, node_ping
+
+# Note, sudo is required to run this script.
+config = toml.load("./db.toml")['psql']
+conn = psycopg2.connect(
+    host=config['host'],
+    port=config['port'],
+    database=config['database'],
+    user=config['user'],
+    password=config['password'],
+)
+
+start, end = node_time.get_time_range(conn)
+on = node_classification.get_on_nodes(conn, start, end)
+pings = node_ping.check_node_ping(conn, on)
+
+counts = {"succeed": 0, "failed": 0}
+sum = 0
+for _, ping in pings.items():
+    if ping:
+        counts['succeed'] += 1
+    else:
+        counts['failed'] += 1
+
+# Plot
+plt.rc('font', size=8)
+plt.pie(counts.values(), labels=counts.keys(), autopct="%.1f%%")
+plt.title("On nodes ping successful from %s to %s" % (start.replace(microsecond=0), end.replace(microsecond=0)))
+plt.show()
