928892fb39ba0c82f03996f0eca4ead8c8f1f074
Dennis Trautwein <git@dtrautwein.eu>
2021-09-22 14:10:50 +0200

    Format SQL migrations

--- migrations/000005_extend_peer_properties_field_lengths.down.sql
+++ migrations/000005_extend_peer_properties_field_lengths.down.sql
@@ -1,2 +1,4 @@
-ALTER TABLE peer_properties ALTER COLUMN value TYPE VARCHAR(255);
-ALTER TABLE peer_properties ALTER COLUMN property TYPE VARCHAR(255);
+ALTER TABLE peer_properties
+    ALTER COLUMN value TYPE VARCHAR(255);
+ALTER TABLE peer_properties
+    ALTER COLUMN property TYPE VARCHAR(255);
--- migrations/000006_add_session_failure_reason.down.sql
+++ migrations/000006_add_session_failure_reason.down.sql
@@ -1,5 +1,7 @@
-ALTER TABLE sessions DROP CONSTRAINT con_finish_reason_not_null_for_finished;
-ALTER TABLE sessions DROP COLUMN finish_reason;
+ALTER TABLE sessions
+    DROP CONSTRAINT con_finish_reason_not_null_for_finished;
+ALTER TABLE sessions
+    DROP COLUMN finish_reason;
 
 DROP TYPE dial_error;
 
