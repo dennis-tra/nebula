46227a918b7bd9d1ec9fd4553bd95906fa8f381e
Dennis Trautwein <git@dtrautwein.eu>
2021-09-22 14:10:50 +0200

    Format SQL migrations

--- migrations/000003_create_peer_properties_table.up.sql
+++ migrations/000003_create_peer_properties_table.up.sql
@@ -1,14 +1,16 @@
-CREATE TABLE IF NOT EXISTS peer_properties (
-    id SERIAL PRIMARY KEY,
-    property VARCHAR(255) NOT NULL,
-    value VARCHAR(255) NOT NULL,
-    count INT NOT NULL,
-    crawl_id SERIAL NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL
+CREATE TABLE IF NOT EXISTS peer_properties
+(
+    id         SERIAL PRIMARY KEY,
+    property   VARCHAR(255) NOT NULL,
+    value      VARCHAR(255) NOT NULL,
+    count      INT          NOT NULL,
+    crawl_id   SERIAL       NOT NULL,
+    created_at TIMESTAMPTZ  NOT NULL,
+    updated_at TIMESTAMPTZ  NOT NULL
 );
 
-ALTER TABLE peer_properties ADD CONSTRAINT fk_peer_property_crawl
-    FOREIGN KEY (crawl_id)
-    REFERENCES crawls (id)
-    ON DELETE CASCADE;
+ALTER TABLE peer_properties
+    ADD CONSTRAINT fk_peer_property_crawl
+        FOREIGN KEY (crawl_id)
+            REFERENCES crawls (id)
+            ON DELETE CASCADE;
--- migrations/000005_extend_peer_properties_field_lengths.up.sql
+++ migrations/000005_extend_peer_properties_field_lengths.up.sql
@@ -1,3 +1,5 @@
 -- Increase column sizes as agent version strings can become quite long.
-ALTER TABLE peer_properties ALTER COLUMN value TYPE VARCHAR(500);
-ALTER TABLE peer_properties ALTER COLUMN property TYPE VARCHAR(500);
+ALTER TABLE peer_properties
+    ALTER COLUMN value TYPE VARCHAR(500);
+ALTER TABLE peer_properties
+    ALTER COLUMN property TYPE VARCHAR(500);
