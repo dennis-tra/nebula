a84dc94593d234c6c2e6746db6d8b83ffbe3ed3a
Dennis Trautwein <git@dtrautwein.eu>
2021-09-23 11:41:10 +0200

    Add separate multi addresses table

---
+++ migrations/000017_create_peers_multi_addresses_join_table.up.sql
@@ -0,0 +1,22 @@
+-- Begin the transaction
+BEGIN;
+
+-- The `peers_multi_addresses` table keeps track of
+-- the association of multi addresses to peers.
+CREATE TABLE peers_multi_addresses
+(
+    -- The peer ID of which we want to track the multi address
+    peer_id  SERIAL,
+    -- The ID of the multi address that has been seen for the above peer
+    maddr_id SERIAL,
+
+    -- The peer ID should always point to an existing peer in the DB
+    CONSTRAINT fk_peers_multi_addresses_peer FOREIGN KEY (peer_id) REFERENCES peers (id) ON DELETE CASCADE,
+    -- The maddr ID should always point to an existing multi address in the DB
+    CONSTRAINT fk_peers_multi_addresses_maddr FOREIGN KEY (maddr_id) REFERENCES multi_addresses (id) ON DELETE CASCADE,
+
+    PRIMARY KEY (peer_id, maddr_id)
+);
+
+-- End the transaction
+COMMIT;
