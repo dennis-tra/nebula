e917f1e06ea389708bf6709856ea2e609d4bca00
Dennis Trautwein <git@dtrautwein.eu>
2021-09-23 11:54:48 +0200

    Add multi address migration to new table

--- migrations/000016_create_multi_addresses_table.up.sql
+++ migrations/000016_create_multi_addresses_table.up.sql
@@ -8,11 +8,13 @@ CREATE TABLE multi_addresses
     -- A unique id that identifies this multi_address
     id             SERIAL,
     -- The multi address in the form of `/ip4/123.456.789.123/tcp/4001`
-    address        VARCHAR(100) NOT NULL,
+    maddr          VARCHAR(200) NOT NULL,
+    -- The derived IPv4 or IPv6 address that was used to determine the country/cloud provider
+    addr           INET,
     -- The country that this multi address belongs to in the form of a two to three letter country code
-    country        VARCHAR(3)   NOT NULL,
+    country        VARCHAR(3),
     -- The cloud provider that this multi address can be associated with
-    cloud_provider VARCHAR(16)  NOT NULL,
+    cloud_provider VARCHAR(16),
 
     -- When was this multi address updated the last time
     updated_at     TIMESTAMPTZ  NOT NULL,
@@ -20,12 +22,12 @@ CREATE TABLE multi_addresses
     created_at     TIMESTAMPTZ  NOT NULL,
 
     -- There should only ever be distinct multi addresses here
-    CONSTRAINT uq_multi_addresses_address UNIQUE (address),
+    CONSTRAINT uq_multi_addresses_address UNIQUE (maddr),
 
     PRIMARY KEY (id)
 );
 
-CREATE INDEX idx_multi_addresses_address ON multi_addresses (address);
+CREATE INDEX idx_multi_addresses_maddr ON multi_addresses (maddr);
 
 -- End the transaction
 COMMIT;
---
+++ migrations/000018_migrate_multi_addresses.down.sql
@@ -0,0 +1,29 @@
+-- Begin the transaction
+BEGIN;
+
+-- Add dropped columns
+ALTER TABLE peers
+    ADD COLUMN multi_addresses VARCHAR(255) ARRAY;
+ALTER TABLE peers
+    ADD COLUMN old_multi_addresses VARCHAR(255) ARRAY;
+
+-- Migrate multi addresses back to peer
+UPDATE peers
+SET multi_addresses=subquery.maddrs
+FROM (SELECT pma.peer_id AS "peer_id", array_agg(maddr) AS "maddrs"
+      FROM multi_addresses ma
+               INNER JOIN peers_multi_addresses pma ON pma.maddr_id = ma.id
+               INNER JOIN peers p ON p.id = pma.peer_id
+      GROUP BY pma.peer_id) AS subquery
+WHERE peers.id = subquery.peer_id;
+
+-- Delete all rows from association table
+DELETE
+FROM peers_multi_addresses;
+
+-- Delete all rows from multi_addresses table
+DELETE
+FROM multi_addresses;
+
+-- End the transaction
+COMMIT;
