3502881ae2d90b9d3715d449d13b700bc0d1097e
Dennis Trautwein <git@dtrautwein.eu>
2021-10-22 16:20:49 +0200

    Add tests for queue package

---
+++ pkg/queue/chan_test.go
@@ -0,0 +1,51 @@
+package queue
+
+import (
+	"runtime"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_FIFO(t *testing.T) {
+	fifo := NewFIFO()
+	for i := 0; i < 10; i++ {
+		fifo.Push(i)
+	}
+	for i := 0; i < 10; i++ {
+		assert.Equal(t, i, <-fifo.Consume())
+	}
+	for i := 0; i < 10; i++ {
+		fifo.Push(i)
+	}
+	fifo.DoneProducing()
+	for i := 0; i < 10; i++ {
+		assert.Equal(t, i, <-fifo.Consume())
+	}
+}
+
+func Test_FIFO_async(t *testing.T) {
+	fifo := NewFIFO()
+
+	var wg sync.WaitGroup
+	wg.Add(10)
+	go func() {
+		for i := 0; i < 10; i++ {
+			assert.Equal(t, i, <-fifo.Consume())
+			wg.Done()
+		}
+	}()
+
+	runtime.Gosched()
+
+	for i := 0; i < 10; i++ {
+		fifo.Push(i)
+	}
+
+	wg.Wait()
+
+	fifo.Push(1)
+	fifo.DoneProducing()
+	assert.Equal(t, 1, <-fifo.Consume())
+}
