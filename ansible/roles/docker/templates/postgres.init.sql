3c196065233833be1a426ce2fa169d6a8b5ae539
Dennis Trautwein <git@dtrautwein.eu>
2021-11-21 15:34:16 +0100

    Add ansible deployment playbook

---
+++ ansible/roles/docker/templates/postgres.init.sql
@@ -0,0 +1,14 @@
+{% for network in networks %}
+-- Create write user for the {{ network.name }} network
+CREATE USER nebula_{{ network.name }} WITH LOGIN PASSWORD '{{ network.db_password }}';
+CREATE DATABASE nebula_{{ network.name }};
+GRANT ALL PRIVILEGES ON DATABASE nebula_{{ network.name }} TO nebula_{{ network.name }};
+
+-- Create read-only user for the {{ network.name }} network
+CREATE USER nebula_{{ network.name }}_read WITH LOGIN PASSWORD '{{ network.db_password_read }}';
+GRANT CONNECT ON DATABASE nebula_{{ network.name }} TO nebula_{{ network.name }}_read;
+GRANT USAGE ON SCHEMA public TO nebula_{{ network.name }}_read;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO nebula_{{ network.name }}_read;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO nebula_{{ network.name }}_read;
+
+{% endfor %}
