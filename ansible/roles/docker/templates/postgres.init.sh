ea98355b2d21ad1a0f69131e5b61b65ecb705700
Dennis Trautwein <git@dtrautwein.eu>
2021-11-26 16:05:54 +0100

    Update postgres initialization script

---
+++ ansible/roles/docker/templates/postgres.init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+set -u
+
+echo "Creating multiple databases"
+
+{% for network in networks %}
+
+echo "Configuring database for the {{ network.name }} network"
+echo "Creating database users"
+
+psql -U "{{ db_user }}" <<-EOSQL
+    CREATE USER nebula_{{ network.name }} WITH LOGIN PASSWORD '{{ network.db_password }}';
+    CREATE USER nebula_{{ network.name }}_read WITH LOGIN PASSWORD '{{ network.db_password_read }}';
+EOSQL
+
+echo "  Creating database for the {{ network.name }} network and granting rights to nebula_{{ network.name }}..."
+psql --echo-all -v ON_ERROR_STOP=1 -U "{{ db_user }}" {{ db_name }} <<-EOSQL
+    CREATE DATABASE nebula_{{ network.name }};
+    GRANT ALL PRIVILEGES ON DATABASE nebula_{{ network.name }} TO nebula_{{ network.name }};
+EOSQL
+
+echo "  Creating read-only user for the {{ network.name }} network..."
+psql --echo-all -v ON_ERROR_STOP=1 -U "{{ db_user }}" -d nebula_{{ network.name }} <<-EOSQL
+  GRANT CONNECT ON DATABASE nebula_{{ network.name }} TO nebula_{{ network.name }}_read;
+  GRANT USAGE ON SCHEMA public TO nebula_{{ network.name }}_read;
+  GRANT SELECT ON ALL TABLES IN SCHEMA public TO nebula_{{ network.name }}_read;
+  ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO nebula_{{ network.name }}_read;
+EOSQL
+
+{% endfor %}
+
+echo "Done!"
+
