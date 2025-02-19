eff40c7706c8a4f46c85133ba79361d1555b97c0
Dennis Trautwein <git@dtrautwein.eu>
2025-02-19 09:13:09 +0100

    clickhouse: store kad ID prefix of peer IDs only
    Storing plain peer IDs increases the storage requirements massively

--- db/migrations/ch/000004_create_neighbors_table.up.sql
+++ db/migrations/ch/000004_create_neighbors_table.up.sql
@@ -1,9 +1,8 @@
 CREATE TABLE neighbors
 (
-    crawl_id    UUID,
-    peer_id     String,
-    neighbor_id String,
-    error_bits  UInt16
+    crawl_id               UUID,
+    peer_kad_id_prefix     UInt64,
+    neighbor_kad_id_prefix UInt64,
+    error_bits             UInt16
 ) ENGINE MergeTree()
-      PRIMARY KEY (crawl_id, peer_id, neighbor_id)
-
+      PRIMARY KEY (crawl_id, peer_kad_id_prefix, neighbor_kad_id_prefix)
